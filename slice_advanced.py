51a6eec05ac03c9a14667643104a646d0e706e78
Lara2910-z <lzarantonello@mail.austral.edu.ar>
2025-04-08 18:02:42 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,7 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-
+    word = input("Ingrese un texto o número:")
+    print(word[4::2])
+    
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
